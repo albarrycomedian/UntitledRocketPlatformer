9d7c63dc9a951922a3a8d1a71e55cbe6f9990d3d
Pl4y3r1 <chris.g.barry@outlook.com>
2023-04-01 10:16:02 -0700

    Fixing bug in debug script, made disable colliders recursive, added enable colliders

--- Assets/Scripts/CheatDebug.cs
+++ Assets/Scripts/CheatDebug.cs
@@ -8,11 +8,14 @@ public class CheatDebug : MonoBehaviour
     private BoxCollider boxCollider;
     private LevelHandler levelHandler;
     private GameObject canvas;
+    private bool collidersEnabled;
 
     private void Start(){
         canvas = GameObject.Find(Constants.CANVAS_NAME);
         levelHandler = canvas.GetComponent<LevelHandler>();
         boxCollider = GetComponent<BoxCollider>();
+        collidersEnabled = true;
+        enableColliders(gameObject);
     }
 
     private void Update() {
@@ -28,18 +31,44 @@ public class CheatDebug : MonoBehaviour
 
      private void toggleCollisions(){
         if(Input.GetKeyDown(KeyCode.C)){
+            if(collidersEnabled){
+                disableColliders(gameObject);
+            } else{
+                enableColliders(gameObject);
+            }
             boxCollider.enabled = !boxCollider.enabled;
-            disableColliders();
         } 
     }
 
-    private void disableColliders(){
-         for(int i = 0; i < transform.childCount; i++){
+    private void disableColliders(GameObject gameObject){
+        for(int i = 0; i < gameObject.transform.childCount; i++){
+            
+            if(gameObject.transform.GetChild(i).childCount > 0){
+                disableColliders(gameObject.transform.GetChild(i).gameObject);
+            }
+
             if(transform.GetChild(i).GetComponent<BoxCollider>() != null){
                 transform.GetChild(i).GetComponent<BoxCollider>().enabled = false;
             } else if(transform.GetChild(i).GetComponent<CapsuleCollider>() != null){
                 transform.GetChild(i).GetComponent<CapsuleCollider>().enabled = false;
             }
         }
+        collidersEnabled = false;
+    }
+
+    private void enableColliders(GameObject gameObject){
+        for(int i = 0; i < gameObject.transform.childCount; i++){
+
+            if(gameObject.transform.GetChild(i).childCount > 0){
+                enableColliders(gameObject.transform.GetChild(i).gameObject);
+            }
+
+            if(transform.GetChild(i).GetComponent<BoxCollider>() != null){
+                transform.GetChild(i).GetComponent<BoxCollider>().enabled = true;
+            } else if(transform.GetChild(i).GetComponent<CapsuleCollider>() != null){
+                transform.GetChild(i).GetComponent<CapsuleCollider>().enabled = true;
+            }
+        }
+        collidersEnabled = true;
     }
 }
